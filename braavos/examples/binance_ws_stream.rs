b89d9ea57adc65572af78b965291bb448d5034d1
chandlersong <chandler605@gmail.com>
2024-09-27 13:10:29 +0800

    完成一个常规的订阅流程

--- braavos/examples/binance_ws_stream.rs
+++ braavos/examples/binance_ws_stream.rs
@@ -1,32 +1,71 @@
 use braavos::binance::bn_models::WsMethod::SUBSCRIBE;
 use braavos::binance::bn_ws_commands::WsRequest;
+use braavos::utils::setup_logger;
 use futures_util::{SinkExt, StreamExt};
+use log::{info, LevelFilter};
+use std::sync::Arc;
+use tokio::sync::Barrier;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
 async fn main() {
+    let _ = setup_logger(Some(LevelFilter::Debug));
     let url = "wss://stream.binance.com:9443/ws/bbb";
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-    println!("WebSocket handshake has been successfully completed");
+    info!("WebSocket handshake has been successfully completed");
     let (mut write, mut read) = ws_stream.split();
-    //
+
     // let subscribe_message = r#"{ "method": "SUBSCRIBE", "params": ["btcusdt@aggTrade"], "id": 1 }"#;
     let params = Some(vec!["btcusdt@aggTrade".to_string(), "btcusdt@depth".to_string()]);
-    let ping_request = WsRequest::new(SUBSCRIBE, params);
-    let request_body = ping_request.to_json();
+    let subscribe_request = WsRequest::new(SUBSCRIBE, params);
+    let request_body = subscribe_request.to_json();
     println!("request body is {}", request_body);
     let msg = Message::Text(request_body);
     if let Err(e) = write.send(msg).await {
         eprintln!("Error while sending message: {}", e);
     }
-    //
-    while let Some(message) = read.next().await {
-        match message {
-            Ok(msg) => println!("Received: {}", msg),
-            Err(e) => eprintln!("Error: {}", e),
+
+    let barrier = Arc::new(Barrier::new(2));
+    let barrier_clone = barrier.clone();
+
+    tokio::spawn(async move {
+        while let Some(message) = read.next().await {
+            if let Ok(msg) = message {
+                match msg {
+                    Message::Text(txt) => {
+                        info!("Received: {}", txt);
+                    }
+                    Message::Ping(ping) => {
+                        // Respond to Ping messages with Pong
+                        let ping_text = String::from_utf8_lossy(&ping).to_string();
+                        info!("收到ping消息:{:?}", ping_text);
+                        if write.send(Message::Pong(ping)).await.is_err() {
+                            info!("Failed to send Pong");
+                            return;
+                        }
+                        info!("发送pong消息");
+                    }
+                    Message::Pong(_) => {
+                        // Optionally handle Pong messages
+                        println!("Received Pong");
+                    }
+                    Message::Close(_) => {
+                        // Handle close messages if needed
+                        info!("Received Close message");
+                        barrier_clone.wait().await;
+                        return;
+                    }
+                    a => {
+                        info!("收到其他消息,{:?}",a);
+                    }
+                }
+            }
         }
-    }
+    });
+    // 主线程等待 Barrier
+    barrier.wait().await;
+    println!("主线程收到信号,继续执行");
 }
 
 
