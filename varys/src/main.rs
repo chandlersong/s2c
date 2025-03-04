a8fe5650a7fb736bf427c71a41a89425be2b4b32
chandlersong <chandler605@gmail.com>
2025-03-04 16:30:14 +0800

    update log

--- varys/src/main.rs
+++ varys/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use log::{info, LevelFilter};
+use maester::tools::logs::setup_logger;
+use std::time::Duration;
+use tokio::signal;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let _ = setup_logger(Some(LevelFilter::Debug));
+    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+
+    // 启动后台任务
+    tokio::spawn(async move {
+        loop {
+            info!("Running at {}", chrono::Local::now());
+            sleep(Duration::from_secs(1)).await;
+            if let Ok(()) = rx.try_recv() {
+                break;
+            }
+        }
+    });
+
+    // 等待关闭信号
+    signal::ctrl_c().await?;
+    info!("Received shutdown signal, sending exit...");
+    tx.send(()).await.unwrap();
+    Ok(())
 }
