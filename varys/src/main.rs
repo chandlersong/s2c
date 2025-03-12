a07dfa4a181fd8b7a11c33b796feb6f8dc966d78
chandlersong <chandler605@gmail.com>
2025-03-12 10:56:12 +0800

    加入默认核心数

--- varys/src/main.rs
+++ varys/src/main.rs
@@ -10,7 +10,7 @@ use std::time::Duration;
 use tokio::signal;
 use tokio::time::sleep;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 100)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let _ = setup_logger(Some(LevelFilter::Debug));
