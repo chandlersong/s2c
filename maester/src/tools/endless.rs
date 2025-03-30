b221ed74530984467fd3f1326ab762e70e923580
chandlersong <chandler605@gmail.com>
2025-03-30 22:18:15 +0800

    给endless加入全局停止的代码

--- maester/src/tools/endless.rs
+++ maester/src/tools/endless.rs
@@ -1,40 +1,78 @@
 //主要是提供一些不断循环的宏
 
+use log::info;
+use tokio::sync::OnceCell;
+
 ///
 /// 不停的调用各种异步运行的方法
 ///
 ///
+use tokio::sync::broadcast;
 
+
+static STOP_NOTIFICATION: OnceCell<broadcast::Sender<()>> = OnceCell::const_new();
+
+
+async fn initial_stop_notification() -> broadcast::Sender<()> {
+    let (tx, _) = broadcast::channel(1000);
+    tx
+}
+
+pub async fn endless_stop_tx() -> broadcast::Sender<()> {
+    STOP_NOTIFICATION.get_or_init(initial_stop_notification).await.clone()
+}
+
+
+pub async fn stop_endless() {
+    let tx = endless_stop_tx().await;
+    info!("stopped the endless program");
+    tx.send(()).unwrap();
+}
 #[macro_export]
 macro_rules! async_endless {
        ( $start_at:expr,             //循环开始时间
-         $sleep_seconds:expr,         //循环周期
+         $sleep_duration:expr,         //循环周期
          async {$($action:tt)*}       // 循环中做的事情
-         $(,async {$($ctrl_c_stop:tt)* })?  //ctrl+c结束做的事情
+         $(,async {$($stop_action:tt)* })?  //ctrl+c结束做的事情
        ) => {
          use tokio::signal;
          let mut start = $start_at.clone();
-         loop {
-            tokio::select! {
-                     _ = signal::ctrl_c() => {
-                         $(
-                           (async { $($ctrl_c_stop)* }).await;
-                         )?
-                         break;
-                     }
-                     _ = sleep_until(start) => {
-                            (async { $($action)* }).await
-                     }
-            }
-            start = start + Duration::from_secs($sleep_seconds as u64);
-        };
+         let mut rx = endless_stop_tx().await.subscribe();
+         tokio::spawn(
+             async move {
+                 loop {
+                    tokio::select! {
+                             _ = signal::ctrl_c() => {
+                                 $(
+                                   (async { $($stop_action)* }).await;
+                                 )?
+                                 break;
+                             }
+                             _ = rx.recv() => {
+                                 $(
+                                   (async { $($stop_action)* }).await;
+                                 )?
+                                 break;
+                             }
+                             _ = sleep_until(start) => {
+                                    (async { $($action)* }).await
+                             }
+                    }
+                    start = start + $sleep_duration;
+                };
+             }
+         )
     };
 }
 
+
 #[cfg(test)]
 pub mod tests {
+    use crate::tools::endless::{endless_stop_tx, stop_endless};
+    use std::sync::{Arc, Mutex};
     use std::time::Duration;
-    use tokio::time::{sleep_until, Instant};
+    use tokio::time::{sleep, sleep_until, Instant};
+
     ///
     /// 因为这个测试是不间断的跑。所以不用没法一直测试。
     #[ignore]
@@ -42,7 +80,8 @@ pub mod tests {
     async fn test_loop_marco() {
         let mut a = 1;
         async_endless! {
-           Instant::now() + Duration::from_secs(1), 1,
+            Instant::now() + Duration::from_millis(10),
+            Duration::from_millis(100),
             async {
                 println!("Hi,{}",a);
                 a= a+1;
@@ -51,7 +90,27 @@ pub mod tests {
                 println!("stop,{}",a);
                 a= a+1;
             }
-        }
+        };
         println!("Hello, world!{}", a);
+        signal::ctrl_c().await.expect("TODO: panic message");
+    }
+
+
+    #[tokio::test]
+    async fn test_manually_stop() {
+        let value = Arc::new(Mutex::new(1));
+        let clone = value.clone();
+        async_endless! {
+            Instant::now() + Duration::from_millis(10),
+            Duration::from_millis(50),
+            async {
+               *clone.lock().unwrap() += 1;
+            }
+        };
+
+        sleep(Duration::from_millis(200)).await;
+        stop_endless().await;
+        let guard = value.lock().unwrap();
+        assert_eq!(*guard, 5);
     }
 }
