b4beb9f2d03d8342528e5735caf2c2d38beea2f5
chandlersong <chandler605@gmail.com>
2025-08-03 23:59:08 +0800

    add some example

--- braavos/examples/bn_restful_examples.rs
+++ braavos/examples/bn_restful_examples.rs
@@ -9,5 +9,7 @@ use braavos::http_client::init_http_client;
 async fn main() {
     let proxy = Option::from("http://localhost:7891");
     init_http_client(proxy);
-    let _ = execute_ping();
+    let ping_result = execute_ping().await;
+    println!("ping的结果{}", ping_result.is_ok());
+    println!("错误结果{}", ping_result.err().unwrap());
 }
