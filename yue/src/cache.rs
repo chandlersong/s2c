89a692561fea0eb2d6403a83908dfaaede9030ea
chandlersong <chandler605@gmail.com>
2025-08-30 20:17:58 +0800

    refactor module

---
+++ yue/src/cache.rs
@@ -0,0 +1,183 @@
+use crate::tools::FrequencyReducer;
+use async_trait::async_trait;
+#[cfg(test)]
+use mockall::automock;
+use moka::future::Cache;
+use std::collections::HashMap;
+use tokio::sync::broadcast;
+
+#[cfg_attr(test, automock)]
+#[async_trait]
+pub trait DashBoard<T: Send + Sync> {
+    /*
+     像是价格，还有一些乱七八糟的信息这类，计划在缓存作为一个中转站。
+     所以在这里对来类似于一个dashboard
+    */
+    async fn set_value(&mut self, key: String, value: T);
+
+    async fn get_value(&self, key: String) -> Option<T>;
+}
+
+///
+/// 1. 对于Arc的一个封装
+/// 2. 定时更新
+///
+///
+#[cfg_attr(test, automock)]
+#[async_trait]
+pub trait AutoUpdateValue<T: Send + Sync> {
+    async fn get_value(&self) -> Result<T, String>;
+}
+
+/// 这个估计用的不多。
+#[derive(Clone)]
+pub struct RealTimeDashBoard<T: Send + Clone + Sync + 'static> {
+    cache: Cache<String, T>,
+}
+
+impl<T: Send + Clone + Sync + 'static> RealTimeDashBoard<T> {
+    pub fn new() -> Self {
+        RealTimeDashBoard {
+            cache: Cache::new(3000),
+        }
+    }
+}
+
+#[async_trait]
+impl<T: Send + Clone + Sync + 'static> DashBoard<T> for RealTimeDashBoard<T> {
+    async fn set_value(&mut self, key: String, value: T) {
+        self.cache.insert(key, value).await;
+    }
+
+    async fn get_value(&self, key: String) -> Option<T> {
+        self.cache.get(&key).await
+    }
+}
+
+///
+/// 可以不停的调用 set_value,但是缓存的更新，会根据其设定的频率进行更新。
+/// 比如你设定3s更新一次。
+/// 那么1s的时候，设为1，2s的时候，设定为2，3s设定为3。那么1-3s不会存在于缓存中。
+/// 第3s才会更新。
+///
+#[derive(Clone)]
+pub struct FrequencyDashBoard<T: Send + Clone + Sync + 'static> {
+    cache: Cache<String, T>,
+    frequency_mill_seconds: u64,
+    cache_tx: broadcast::Sender<(String, T)>,
+    frequency_reducers: HashMap<String, FrequencyReducer<(String, T)>>,
+}
+
+async fn cache_update<T: Send + Clone + Sync + 'static>(
+    cache: Cache<String, T>,
+    mut cache_rx: broadcast::Receiver<(String, T)>,
+) {
+    loop {
+        if let Ok((key, value)) = cache_rx.recv().await {
+            cache.insert(key, value).await;
+        }
+    }
+}
+
+/// TODO：
+/// 1. 可以配置cache. channel的capacity和cache的都要。
+impl<T: Send + Clone + Sync + 'static> FrequencyDashBoard<T> {
+    #[cfg(test)]
+    pub fn new_for_ut(cache: Cache<String, T>) -> Self {
+        let (tx, _) = broadcast::channel(3000);
+        FrequencyDashBoard {
+            cache,
+            frequency_mill_seconds: 1000,
+            cache_tx: tx,
+            frequency_reducers: HashMap::new(),
+        }
+    }
+
+    pub async fn new(frequency_mill_seconds: u64) -> Self {
+        let (tx, rx) = broadcast::channel(500);
+        let cache: Cache<String, T> = Cache::new(500);
+        let cache_4_update = cache.clone();
+        tokio::spawn(async move {
+            cache_update(cache_4_update, rx).await;
+        });
+        FrequencyDashBoard {
+            cache: cache.clone(),
+            frequency_mill_seconds,
+            cache_tx: tx,
+            frequency_reducers: HashMap::new(),
+        }
+    }
+
+    pub fn get_all_entries(&self) -> Vec<T> {
+        let mut res: Vec<T> = Vec::new();
+        for (_, value) in &self.cache {
+            res.push(value.clone());
+        }
+        res
+    }
+}
+
+#[async_trait]
+impl<T: Send + Clone + Sync + 'static> DashBoard<T> for FrequencyDashBoard<T> {
+    async fn set_value(&mut self, key: String, value: T) {
+        let frequency_reducer = self.frequency_reducers.get(&key).cloned();
+        match frequency_reducer {
+            Some(mut reducer) => {
+                reducer.update((key, value)).await;
+            }
+            None => {
+                let mut new_frequency_reducer =
+                    FrequencyReducer::new(self.cache_tx.clone(), self.frequency_mill_seconds).await;
+                new_frequency_reducer.update((key.clone(), value)).await;
+                self.frequency_reducers.insert(key, new_frequency_reducer);
+            }
+        }
+    }
+
+    async fn get_value(&self, key: String) -> Option<T> {
+        self.cache.get(&key).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cache::{DashBoard, FrequencyDashBoard, RealTimeDashBoard};
+    use std::time::Duration;
+    use tokio::time::sleep;
+
+    #[tokio::test]
+    async fn test_bn_realtime_board() {
+        let mut dashboard = RealTimeDashBoard::new();
+
+        dashboard.set_value("a1".to_string(), 1).await;
+
+        let actual = dashboard.get_value("a1".to_string()).await;
+        assert_eq!(1, actual.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_bn_frequency_board_initial() {
+        let mut dashboard = FrequencyDashBoard::new(100).await;
+
+        dashboard.set_value("a1".to_string(), 1).await;
+        let none_value = dashboard.get_value("a1".to_string()).await;
+        assert_eq!(None, none_value); //测试没有更新
+        sleep(Duration::from_millis(200)).await;
+        let actual = dashboard.get_value("a1".to_string()).await;
+        assert_eq!(1, actual.unwrap()); //测试初始化
+    }
+
+    #[tokio::test]
+    async fn test_bn_frequency_board_replace() {
+        let mut dashboard = FrequencyDashBoard::new(100).await;
+
+        dashboard.set_value("a1".to_string(), 1).await;
+        sleep(Duration::from_millis(200)).await;
+        let actual = dashboard.get_value("a1".to_string()).await;
+        assert_eq!(1, actual.unwrap());
+        dashboard.set_value("a1".to_string(), 2).await;
+        sleep(Duration::from_millis(200)).await;
+        let second_value = dashboard.get_value("a1".to_string()).await;
+        assert_eq!(2, second_value.unwrap());
+    }
+}
---
+++ yue/src/websockets.rs
@@ -0,0 +1,193 @@
+use crate::errors::BraavosError;
+use futures_util::{SinkExt, StreamExt};
+use log::{debug, error, info, trace};
+use std::error::Error;
+use std::sync::Arc;
+use tokio::sync::{RwLock, broadcast, mpsc, oneshot};
+use tokio::time::{Duration, sleep, timeout};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+
+#[derive(Debug, Clone)]
+pub enum ResponseCode {
+    Ok,
+    Failed,
+    TimedOut,
+}
+
+#[derive(Clone)]
+pub struct WebSocketConfig {
+    reconnect_duration: Duration,
+}
+
+impl Default for WebSocketConfig {
+    fn default() -> Self {
+        Self {
+            reconnect_duration: Duration::from_secs(5),
+        }
+    }
+}
+
+type TextMessageSender = Arc<RwLock<Option<broadcast::Sender<String>>>>;
+#[derive(Clone)]
+pub struct WebSocketClient {
+    url: String,
+    text_message_tx: Arc<RwLock<Option<broadcast::Sender<String>>>>,
+    command_tx: mpsc::Sender<(Message, oneshot::Sender<ResponseCode>)>,
+    config: WebSocketConfig,
+    connected_tx: broadcast::Sender<()>,
+}
+
+impl WebSocketClient {
+    pub async fn new(url: &str, config: Option<WebSocketConfig>) -> Result<Self, Box<dyn Error>> {
+        let (tx, rx) = mpsc::channel::<(Message, oneshot::Sender<ResponseCode>)>(1000);
+        let (connected_tx, _) = broadcast::channel(100);
+        let real_config = config.unwrap_or_default();
+        let client = WebSocketClient {
+            url: url.to_string(),
+            text_message_tx: Arc::new(RwLock::new(None)),
+            command_tx: tx,
+            config: real_config,
+            connected_tx,
+        };
+
+        client.start_connection(rx).await;
+        Ok(client)
+    }
+
+    pub fn subscribe_connected(&self) -> broadcast::Sender<()> {
+        self.connected_tx.clone()
+    }
+
+    pub async fn subscribe_text_message_sender(&self) -> broadcast::Receiver<String> {
+        if let Some(sender) = self.text_message_tx.read().await.as_ref() {
+            return sender.subscribe();
+        };
+
+        let (tx, rx) = broadcast::channel(100);
+        *self.text_message_tx.write().await = Some(tx);
+        rx
+    }
+
+    async fn start_connection(
+        &self,
+        mut rx: mpsc::Receiver<(Message, oneshot::Sender<ResponseCode>)>,
+    ) {
+        let url = self.url.clone();
+        let tx = self.command_tx.clone();
+        let reconnect_duration = self.config.reconnect_duration.clone();
+        let text_message_tx = self.text_message_tx.clone();
+
+        let connected_tx = self.connected_tx.clone();
+        tokio::spawn(async move {
+            loop {
+                match Self::run_connection(&url, &text_message_tx, &tx, &mut rx, &connected_tx)
+                    .await
+                {
+                    Ok(()) => info!("WebSocket closed normally"),
+                    Err(e) => error!("WebSocket error: {}", e),
+                }
+                info!("Reconnecting in 5 seconds...");
+                sleep(reconnect_duration).await;
+            }
+        });
+    }
+
+    async fn run_connection(
+        url: &str,
+        text_message_tx: &TextMessageSender,
+        _: &mpsc::Sender<(Message, oneshot::Sender<ResponseCode>)>, //为扩展准备。可以发送命令
+        command: &mut mpsc::Receiver<(Message, oneshot::Sender<ResponseCode>)>,
+        notify: &broadcast::Sender<()>,
+    ) -> Result<(), Box<dyn Error>> {
+        let (ws_stream, _) = connect_async(url).await?;
+        info!("WebSocket connected to {}", url);
+        let (mut write, mut read) = ws_stream.split();
+        //觉得没有什么好处理的。错误也是别人没有听到。
+        match notify.send(()) {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+        let ping_interval = Duration::from_secs(10);
+        loop {
+            tokio::select! {
+                    // 处理接收消息
+                    Some(msg) = read.next() => {
+                        match msg {
+                            Ok(Message::Text(text)) => {
+                                if let Some(sender)= text_message_tx.read().await.as_ref() {
+                                    match sender.send(text){
+                                        Ok(..) => {},
+                                        Err(e) => error!("发送文字消息失败: {}", e),
+                                    };
+                                };
+                            }
+                            Ok(Message::Ping(data)) => {
+                                trace!("Received Ping: {:?}", data);
+                            }
+                            Ok(Message::Pong(_)) => {
+                                 trace!("Received Pong");
+                            }
+                            Ok(Message::Close(_)) => {
+                                debug!("Server closed connection");
+                                return Ok(());
+                            }
+                            Ok(_) => {} // 忽略其他类型
+                            Err(e) => {
+                                return Err(Box::new(e));
+                            }
+                        }
+
+                    }
+
+                // 处理发送消息或 Ping
+                Some((msg, tx)) = command.recv() => {
+                    match timeout(Duration::from_secs(1), write.send(msg)).await {
+                        Ok(result) => match result {
+                            Ok(_) => {
+                                match tx.send(ResponseCode::Ok) {
+                                    Ok(_) => {}
+                                    Err(e) => { error!("error while sending ok message: {:?}",  e); }
+                                }
+                            }
+                            Err(e) => {
+                                    error!("error while sending ok message: {:?}", e);
+                                    match tx.send(ResponseCode::Failed) {
+                                        Ok(_) => {}
+                                        Err(e) => { error!("error while sending error notification: {:?}",  e); }
+                                    }
+                            }
+                        },
+                        Err(_) => {
+                            match tx.send(ResponseCode::TimedOut) {
+                                Ok(_) => {}
+                                Err(e) => { error!("error while sending timeout notification: {:?}",  e); }
+                            }
+                        }
+                    }
+                }
+                _ = sleep(ping_interval) => {
+                    let ping = Message::Ping(vec![1, 2, 3]);
+                    write.send(ping).await?;
+                    debug!("Sent Ping");
+                }
+            }
+        }
+    }
+
+    pub async fn send(&self, message: Message) -> Result<(), Box<dyn Error>> {
+        let (tx, rx) = oneshot::channel();
+        self.command_tx.send((message, tx)).await?;
+
+        match rx.await {
+            Ok(result) => match result {
+                ResponseCode::Ok => Ok(()),
+                ResponseCode::Failed => Err(Box::new(BraavosError::new("request error"))),
+                ResponseCode::TimedOut => Err(Box::new(BraavosError::new("request timeout error"))),
+            },
+            Err(error) => {
+                error!("request error: {}", error);
+                Err(Box::new(BraavosError::new("request error")))
+            }
+        }
+    }
+}
