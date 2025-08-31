d90f5787c9a681402a85f62a759e114d0fd9e6c7
chandlersong <chandler605@gmail.com>
2025-08-31 15:09:35 +0800

    修改错误

--- yue/src/errors.rs
+++ yue/src/errors.rs
@@ -1,48 +1,20 @@
 use hmac::digest::InvalidLength;
-use std::error::Error;
-use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug)]
-pub struct YueError {
-    message: String,
-}
-
-// 实现 Display trait，用于将错误信息格式化为字符串
-impl fmt::Display for YueError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Custom Error: {}", self.message)
-    }
+#[derive(Error, Debug)]
+pub enum YueError {
+    #[error("Request error: {0}")]
+    RequestError(#[from] ureq::Error),
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("Invalid key length for HMAC: {0}")]
+    InvalidKeyLength(#[from] InvalidLength),
+    #[error("Custom error: {0}")]
+    CustomError(String),
 }
 
 impl YueError {
     pub fn new(message: &str) -> YueError {
-        YueError {
-            message: message.to_string(),
-        }
-    }
-}
-
-// 实现 Error trait，用于提供错误信息
-impl Error for YueError {}
-
-impl From<ureq::Error> for YueError {
-    fn from(error: ureq::Error) -> Self {
-        YueError {
-            message: format!("request Error: {}", error),
-        }
-    }
-}
-
-impl From<std::io::Error> for YueError {
-    fn from(error: std::io::Error) -> Self {
-        YueError {
-            message: format!("request Error: {}", error),
-        }
-    }
-}
-
-impl From<InvalidLength> for YueError {
-    fn from(err: InvalidLength) -> Self {
-        YueError::new(&format!("Invalid key length for HMAC: {}", err))
+        YueError::CustomError(message.to_string())
     }
 }
