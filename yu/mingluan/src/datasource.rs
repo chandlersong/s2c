137d739ac58ec52f581a3f93ab06c6d9d4c23dc6
chandlersong <chandler605@gmail.com>
2025-09-07 22:08:31 +0800

    完成duckdb的连接池

---
+++ yu/mingluan/src/datasource.rs
@@ -0,0 +1,25 @@
+use duckdb::DuckdbConnectionManager;
+use r2d2;
+use r2d2::Pool;
+use std::sync::OnceLock;
+
+pub(crate) static CONNECTION_POOL: OnceLock<Pool<DuckdbConnectionManager>> = OnceLock::new();
+
+pub fn get_connection_pool() -> &'static Pool<DuckdbConnectionManager> {
+    //TODO：像超时这类进行配置。
+
+    CONNECTION_POOL.get_or_init(|| {
+        /* TODO
+           1， 目前是内存数据库，改成文件数据库。并且位置从配置文件读取
+           2.  修改配置项，比如设置多大的链接痴
+        */
+        let builder = r2d2::Pool::builder()
+            .max_size(15) // 最大连接数
+            .min_idle(Some(5)) // 最小空闲连接数
+            .connection_timeout(std::time::Duration::from_secs(5)); // 连接超时时间
+
+        builder
+            .build(DuckdbConnectionManager::memory().unwrap())
+            .unwrap()
+    })
+}
--- yu/mingluan/src/main.rs
+++ yu/mingluan/src/main.rs
@@ -1,3 +1,52 @@
+use crate::datasource::get_connection_pool;
+use duckdb::arrow::array::Datum;
+use duckdb::{Connection, DuckdbConnectionManager, params};
+use std::thread;
+
+pub mod datasource;
+
+struct Duck {
+    id: i32,
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = get_connection_pool().get().unwrap();
+
+    conn.execute(
+        "CREATE TABLE ducks (id INTEGER PRIMARY KEY, name TEXT)",
+        [], // empty list of parameters
+    )
+    .unwrap();
+
+    conn.execute_batch(
+        r#"
+        INSERT INTO ducks (id, name) VALUES (1, 'Donald Duck');
+        INSERT INTO ducks (id, name) VALUES (2, 'Scrooge McDuck');
+        "#,
+    )
+    .unwrap();
+
+    conn.execute(
+        "INSERT INTO ducks (id, name) VALUES (?, ?)",
+        params![3, "Darkwing Duck"],
+    )
+    .unwrap();
+
+    let ducks = conn
+        .prepare("FROM ducks")
+        .unwrap()
+        .query_map([], |row| {
+            Ok(Duck {
+                id: row.get(0)?,
+                name: row.get(1)?,
+            })
+        })
+        .unwrap()
+        .collect::<duckdb::Result<Vec<_>>>()
+        .unwrap();
+
+    for duck in ducks {
+        println!("{}) {}", duck.id, duck.name);
+    }
 }
