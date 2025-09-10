b5c0fcfb0970e35e2c049ceb7f0009feeb3d9c12
chandlersong <chandler605@gmail.com>
2025-09-10 11:44:33 +0800

    处理一部分代码

--- yu/mingluan/src/binance/update_manager.rs
+++ yu/mingluan/src/binance/update_manager.rs
@@ -1,46 +1,19 @@
 use crate::binance::binance_consts::QUERY_LATEST_SQL;
-use crate::exchange::KlineUpDate;
+use crate::exchange::KlineUpdate;
 use duckdb::DuckdbConnectionManager;
 use r2d2::PooledConnection;
-use yue::binance::spots::KlineFetcher;
 
-pub struct SpotKlineRefresh<F>
-where
-    F: KlineFetcher + Send + Sync,
-{
+pub struct SpotKlineRefresh {
     connection: PooledConnection<DuckdbConnectionManager>,
-    kline_fetcher: F,
 }
 
-impl<F> SpotKlineRefresh<F>
-where
-    F: KlineFetcher + Send + Sync,
-{
-    pub fn new(connection: PooledConnection<DuckdbConnectionManager>) -> Self
-    where
-        F: KlineFetcher + Send + Sync + Default,
-    {
-        SpotKlineRefresh {
-            connection,
-            kline_fetcher: F::default(),
-        }
-    }
-
-    pub fn with_fetcher(
-        connection: PooledConnection<DuckdbConnectionManager>,
-        kline_fetcher: F,
-    ) -> Self {
-        SpotKlineRefresh {
-            connection,
-            kline_fetcher,
-        }
+impl SpotKlineRefresh {
+    pub fn new(connection: PooledConnection<DuckdbConnectionManager>) -> Self {
+        SpotKlineRefresh { connection }
     }
 }
 
-impl<F> KlineUpDate for SpotKlineRefresh<F>
-where
-    F: KlineFetcher + Send + Sync,
-{
+impl KlineUpdate for SpotKlineRefresh {
     async fn update(&self) -> String {
         let mut stmt = self.connection.prepare(QUERY_LATEST_SQL).unwrap();
         let latest_symbol = stmt
@@ -69,14 +42,11 @@ where
 mod tests {
     use crate::binance::binance_consts::BinanceTables::SpotKline;
     use crate::binance::update_manager::SpotKlineRefresh;
-    use crate::exchange::KlineUpDate;
+    use crate::exchange::KlineUpdate;
     use crate::test_utils::import_local_csv_and_assert;
-    use async_trait::async_trait;
     use duckdb::DuckdbConnectionManager;
-    use mockall::mock;
     use r2d2::PooledConnection;
     use std::path::Path;
-    use yue::binance::spots::KlineFetcher;
 
     fn initial_db() -> PooledConnection<DuckdbConnectionManager> {
         let builder = r2d2::Pool::builder()
@@ -91,19 +61,19 @@ mod tests {
             .unwrap()
     }
 
-    mock! {
-        pub KlineFetcher {}
-
-        #[async_trait]
-        impl KlineFetcher for KlineFetcher {
-            async fn get_all_kline_data(
-                &self,
-                symbol: &str,
-                interval: yue::binance::spots::KlineInterval,
-                start_time: Option<u64>,
-            ) -> Result<(Vec<yue::binance::spots::Kline>, usize), yue::errors::YueError>;
-        }
-    }
+    // mock! {
+    //     pub KlineFetcher {}
+    //
+    //     #[async_trait]
+    //     impl KlineFetcher for KlineFetcher {
+    //         async fn get_all_kline_data(
+    //             &self,
+    //             symbol: &str,
+    //             interval: yue::binance::spots::KlineInterval,
+    //             start_time: Option<u64>,
+    //         ) -> Result<(Vec<yue::binance::spots::Kline>, usize), yue::errors::YueError>;
+    //     }
+    // }
 
     #[tokio::test]
     async fn test_refresh_spot_kline_normal() {
@@ -114,13 +84,19 @@ mod tests {
         // 直接从仓库中的本地 CSV 导入并断言行数为 2
         let csv_path = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("tests/data/test_refresh_spot_kline_normal.csv");
-        import_local_csv_and_assert(&conn, "spot_kline", csv_path.as_path(), 3).unwrap();
+        import_local_csv_and_assert(
+            &conn,
+            SpotKline.table_name().as_str(),
+            csv_path.as_path(),
+            3,
+        )
+        .unwrap();
 
-        use mockall::predicate::{always, eq};
+        // use mockall::predicate::{always, eq};
+        //
+        // let mut mock = MockKlineFetcher::new();
 
-        let mut mock = MockKlineFetcher::new();
-
-        let manager = SpotKlineRefresh::with_fetcher(conn, mock);
+        let manager = SpotKlineRefresh::new(conn);
         manager.update().await;
     }
 }
--- yu/mingluan/src/exchange.rs
+++ yu/mingluan/src/exchange.rs
@@ -2,7 +2,7 @@
 /// 主要处理各个交易所的数据的更新操作，
 /// 不保存任何交易所的具体操作
 ///
-pub(crate) trait KlineUpDate {
+pub(crate) trait KlineUpdate {
     /// 刷新现货K线
     async fn update(&self) -> String;
 }
