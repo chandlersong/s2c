f46fd739e054065e3d7995d45e707f3b6e782711
chandlersong <chandler605@gmail.com>
2026-02-10 07:23:09 +0800

    fix isse

--- yu/examples/data_integrity_example.rs
+++ yu/examples/data_integrity_example.rs
@@ -98,6 +98,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let supervisor = DataIntegritySupervisor::new_with_config(config, check_strategies, repair_strategies).await;
     supervisor.start();
-    tokio::time::sleep(tokio::time::Duration::from_mins(5)).await;
+    tokio::time::sleep(tokio::time::Duration::from_secs(300)).await;
     Ok(())
 }
