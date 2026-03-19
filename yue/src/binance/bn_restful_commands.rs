87fb2fea6b877320576f07098aa0c22b86a321a2
chandlersong <chandler605@gmail.com>
2026-03-19 17:27:16 +0800

    fix exchange error

--- yue/src/binance/bn_restful_commands.rs
+++ yue/src/binance/bn_restful_commands.rs
@@ -225,7 +225,7 @@ pub static SPOT_DEPTH_1000_COMMAND: LazyLock<RequestInfo> =
 /// SWAP API
 
 pub static SWAP_EXCHANGE_COMMAND: LazyLock<RequestInfo> =
-    LazyLock::new(|| RequestInfo::from_base_path(BINANCE_SPOT_BASE.clone(), SWAP_EXCHANGE_INFO_PATH, false, 20, None, Some(90)).unwrap());
+    LazyLock::new(|| RequestInfo::from_base_path(BINANCE_SWAP_BASE.clone(), SWAP_EXCHANGE_INFO_PATH, false, 20, None, Some(90)).unwrap());
 
 pub static SWAP_FUNDING_RATE_COMMAND: LazyLock<RequestInfo> =
     LazyLock::new(|| RequestInfo::from_base_path(BINANCE_FUNDING_RATE_BASE.clone(), SWAP_FUNDING_RATE_PATH, false, 1, None, Some(60 * 60)).unwrap());
